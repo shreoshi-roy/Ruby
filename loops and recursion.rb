f111c3cedf205cba930e57e60e1c8c7c4a73e2f1
Shreoshi Roy <56217201+shreoshi-roy@users.noreply.github.com>
2020-03-31 23:35:45 +0530

    Update loops and recursion.rb

--- loops and recursion.rb
+++ loops and recursion.rb
@@ -1,1 +1,26 @@
-loops and recursion
+##Find sum of all numbers in an array
+
+# Iterative approach using for loop or each
+def sum1(array)
+  sum = 0
+  array.each do |number|
+    sum += number
+  end
+  sum
+end
+
+sum1([5, 8, 15, 20, 24, 30])
+
+
+# Recursive approach
+def sum2(array)
+  if array? # Base case
+    return 0
+
+  else
+    Sum = array.pop
+    return Sum + sum2(array)
+  end
+end
+
+sum2([5, 8, 15, 20, 24, 30]) 
